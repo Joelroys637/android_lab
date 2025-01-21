11086c56bfd714a641c7ae6b9ccd319f29744859
Joelroys637 <97875782+Joelroys637@users.noreply.github.com>
2025-01-21 18:37:57 +0530

    Update android.py

--- android.py
+++ android.py
@@ -398,80 +398,34 @@ public class MainActivity extends Activity {
 
 st.write("color bg change")
 st.write("Xml code")
-st.code("""package com.example.bounc;
+st.code("""<?xml version="1.0" encoding="utf-8"?>
+<RelativeLayout xmlns:android="http://schemas.android.com/apk/res/android"
+    android:layout_width="match_parent"
+    android:layout_height="match_parent"
+    android:padding="16dp"
+    android:id="@+id/relativelayout1">
 
-import android.app.Activity;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.view.View.OnClickListener;
-import android.view.animation.Animation;
-import android.view.animation.BounceInterpolator;
-import android.view.animation.TranslateAnimation;
-import android.view.animation.Animation.AnimationListener;
-import android.widget.Button;
-import android.widget.ImageView;
+    <Button
+        android:id="@+id/button1"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_alignParentLeft="true"
+        android:layout_alignParentTop="true"
+        android:layout_marginLeft="94dp"
+        android:layout_marginTop="87dp"
+        android:text="@string/cool" 
+        android:onClick="OnClick"/>
 
-public class MainActivity extends Activity {
+    <Button
+        android:id="@+id/button2"
+        android:layout_width="wrap_content"
+        android:layout_height="wrap_content"
+        android:layout_alignRight="@+id/button1"
+        android:layout_centerVertical="true"
+        android:text="Button" />
 
-    private static final String TAG = "AnimationStarter";
+</RelativeLayout>
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        Button bounceBallButton = (Button) findViewById(R.id.bounceBallButton);
-        final ImageView bounceBallImage = (ImageView) findViewById(R.id.bounceBallImage);
-
-        bounceBallButton.setOnClickListener(new OnClickListener() {
-
-            @Override
-            public void onClick(View v) {
-                bounceBallImage.clearAnimation();
-                TranslateAnimation transAnim = new TranslateAnimation(0, 0, 0,
-                        getDisplayHeight()/2);
-                transAnim.setStartOffset(500);
-                transAnim.setDuration(3000);
-                transAnim.setFillAfter(true);
-                transAnim.setInterpolator(new BounceInterpolator());
-                transAnim.setAnimationListener(new AnimationListener() {
-
-                    @Override
-                    public void onAnimationStart(Animation animation) {
-                        Log.i(TAG, "Starting button dropdown animation");
-
-                    }
-
-                    @Override
-                    public void onAnimationRepeat(Animation animation) {
-                        // TODO Auto-generated method stub
-
-                    }
-
-                    @Override
-                    public void onAnimationEnd(Animation animation) {
-                        Log.i(TAG,
-                                "Ending button dropdown animation. Clearing animation and setting layout");
-                        bounceBallImage.clearAnimation();
-                        final int left = bounceBallImage.getLeft();
-                        final int top = bounceBallImage.getTop();
-                        final int right = bounceBallImage.getRight();
-                        final int bottom = bounceBallImage.getBottom();
-                        bounceBallImage.layout(left, top, right, bottom);
-
-                    }
-                });
-                bounceBallImage.startAnimation(transAnim);
-            }
-        });
-
-    }
-
-    private int getDisplayHeight() {
-        return this.getResources().getDisplayMetrics().heightPixels;
-    }
-}
 """)
 
 st.write("change string.xml it's located in res under value folder")
